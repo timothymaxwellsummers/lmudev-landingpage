72a1ed09e8b553351f51b4880d9ac2af478a5fd7
Lukas Plenk <lukasplenk@yahoo.de>
2025-01-24 02:09:09 +0100

    Quick Fix: Added missing reference in mobile header

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -103,7 +103,7 @@ export default function Header() {
                 </div>
                 <div className="py-6">
                   <a
-                    href="#"
+                    href="mailto:contact@lmu-dev.org"
                     className="-mx-3 rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50 inline-flex gap-2"
                   >
                     Contact us{" "}
