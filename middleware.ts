a94f42985ecfeb58876f29f3eeb907b52007fc60
lukasplenk <lukasplenk@yahoo.de>
2025-12-09 15:33:21 +0900

    Added middleware for deep-link redirects if app not installed

---
+++ middleware.ts
@@ -0,0 +1,17 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+const APP_PREFIX = '/app/';
+const APP_LANDING_PAGE = 'https://lmu-app.lmu-dev.org/';
+
+export function middleware(request: NextRequest) {
+  const url = request.nextUrl.clone();
+  
+  if (url.pathname.startsWith(APP_PREFIX)) {
+    url.href = APP_LANDING_PAGE;
+    
+    return NextResponse.redirect(url, 308);
+  }
+
+  return NextResponse.next();
+}
