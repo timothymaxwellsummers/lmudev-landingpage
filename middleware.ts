e40ac11bbe75d0dfd56508f52bf309aec5d7d509
lukasplenk <lukasplenk@yahoo.de>
2025-12-09 15:52:31 +0900

    Updated app prefix in middleware

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
-const APP_PREFIX = 'app/';
+const APP_PREFIX = 'app';
 const APP_LANDING_PAGE = 'https://lmu-app.lmu-dev.org/';
 
 export function middleware(request: NextRequest) {
